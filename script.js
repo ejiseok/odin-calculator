5600bb66a74c4d61a29f737ec843fa76b3d456af
ejiseok <jiseoklee0502@gmail.com>
2026-03-18 20:16:08 +0900

    버튼 클릭 구현, 계산 구현

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const DIVIDE = "÷";
 const LEFT_ARROW = "←";
 const CLEAR = "C";
 const OPERATORS = "+−×÷";
+const EQUAL = "=";
+const DOT = ".";
 const NUMBERS = "0123456789";
 
 
@@ -25,8 +27,8 @@ function divide(num1, num2) {
 }
 
 function operate(operand1, operand2, operator) {
-  let num1 = parseInt(operand1);
-  let num2 = parseInt(operand2);
+  let num1 = Number(operand1);
+  let num2 = Number(operand2);
 
   let result;
   if (operator === PLUS) {
@@ -42,21 +44,103 @@ function operate(operand1, operand2, operator) {
   return result;
 }
 
+function clickNumbers(inputNum) {
+  if (operator === "") {
+    operand1 = parseInt(operand1 + inputNum).toString();
+    display.textContent = operand1;
+  } else {
+    operand2 = parseInt(operand2 + inputNum).toString();
+    display.textContent = `${operand1} ${operator} ${operand2}`;  
+  }
+}
 
-let operand1;
-let operand2;
-let operator;
+function clickOperators(content) {
+  if (operand2 === 0) {
+    operator = content;
+    display.textContent = `${operand1} ${operator}`;
+  } else {
+    operand1 = operate(operand1, operand2, operator);
+    operand2 = 0;
+    operator = content;
+    display.textContent = `${operand1} ${operator}`;
+  }
+}
 
-const buttons = document.querySelectorAll(".btn");
+function clickLeftArrow() {
+  if (operator === "" && operand2 === 0) {
+    let num = operand1.toString();
+    num = num.slice(0, num.length - 1);
+    if (num === "") {
+      operand1 = 0;
+    } else {
+      operand1 = Number(num);
+    }
+    display.textContent = `${operand1}`;
 
-buttons.forEach(button => {
-  button.addEventListener("click", e => {
+  } else if (operator !== "" && operand2 === 0) {
+    operator = "";
+    display.textContent = `${operand1}`;
+
+  } else if (operand2 !== 0) {
+    let num = operand2.toString();
+    num = num.slice(0, num.length - 1);
+    if (num === "") {
+      operand2 = 0;
+      display.textContent = `${operand1} ${operator}`;
+    } else {
+      operand2 = Number(num);
+      display.textContent = `${operand1} ${operator} ${operand2}`;
+    }
+  }
+}
+
+function clickClear() {
+  operand1 = 0;
+  operand2 = 0;
+  operator = "";
+
+  display.textContent = `${operand1}`;
+}
+
+function clickEqual() {
+  if (operand1 !== 0 && operator !== "") {
+    operand1 = operate(operand1, operand2, operator);
+    display.textContent = `${operand1}`;
+  }
+}
+
+function clickDot() {
+  
+}
+
+function clickButton(e) {
     const content = e.target.textContent;
 
     if (NUMBERS.includes(content)) {
-
+      clickNumbers(content);
     } else if (OPERATORS.includes(content)) {
-
+      clickOperators(content);
+    } else if (content === LEFT_ARROW) {
+      clickLeftArrow();
+    } else if (content === CLEAR) {
+      clickClear();
+    } else if (content === EQUAL) {
+      clickEqual();
+    } else if (content === DOT) {
+      clickDot();
     }
-  });
+}
+
+
+let operand1 = 0;
+let operand2 = 0;
+let operator = "";
+
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+  button.addEventListener("click", clickButton);
 });
+
+display.textContent = `${operand1}`;
